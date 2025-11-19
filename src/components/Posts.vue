4a6541dce8ac53553971c692d07925cac87bc370
Sam Gabe <Samgabe1998@gmail.com>
2025-11-19 16:03:28 +0300

    The Post component now is styled with a table

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -17,17 +17,27 @@ onMounted(() => {
 
 <template>
   <div>
-    <h2 class="m-10 font-semibold ">Latest Posts</h2>
+    <h2 class="m-10 font-semibold text-2xl">Latest Posts</h2>
 
-    <p v-if="loading">Loading...</p>
+    <p v-if="loading" class="m-10 text-gray-600">Loading...</p>
 
-    <div  
-      v-for="post in posts" 
-      :key="post.id" 
-      class="m-10 p-4 border border-gray-300 rounded-lg shadow-md"
-    >
-      <h3 class="">{{ post.title }}</h3>
-      <p class="">{{ post.body }}</p>
+    <div v-if="!loading" class="m-10 overflow-x-auto">
+      <table class="w-full border-collapse border border-gray-300 rounded-lg shadow-md">
+        <thead class="bg-blue-500 text-white">
+          <tr>
+            <th class="border border-gray-300 p-3 text-left">ID</th>
+            <th class="border border-gray-300 p-3 text-left">Title</th>
+            <th class="border border-gray-300 p-3 text-left">Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="post in posts" :key="post.id" class="hover:bg-gray-100 transition">
+            <td class="border border-gray-300 p-3">{{ post.id }}</td>
+            <td class="border border-gray-300 p-3 font-semibold">{{ post.title }}</td>
+            <td class="border border-gray-300 p-3">{{ post.body }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
